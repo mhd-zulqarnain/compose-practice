ec36ff6f93262e16dbb106adc97df1f4be559e62
Zulqarnain <m.zulqarnain@purevpn.com>
2022-11-15 20:49:36 +0500

    fixed dependency issue

---
+++ app/src/main/java/com/project/tailor/room/ProductDao.kt
@@ -0,0 +1,12 @@
+package com.project.tailor.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import com.project.tailor.model.Product
+
+@Dao
+class ProductDao {
+
+//    @Insert
+//    fun insertProduct(product: Product)
+}
